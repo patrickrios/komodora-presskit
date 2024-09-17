afd97342199d1417499a2e15e05349f8b44bdede
patrickrios <patrick.33.rios@hotmail.com>
2024-09-17 16:46:46 -0300

    refact: remove page router

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,13 @@ import React from 'react';
 import './App.css'
 import AppProvider from './contexts/AppContext';
 import { ThemeProvider } from './contexts/ThemeContext';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { KomodoraPresskit } from './pages/komodora_presskit/PressKit';
 
 const  App = () =>{
   return(
     <AppProvider>
       <ThemeProvider>
-        <Router>
-          <Routes>
-            <Route path='/' element={<KomodoraPresskit/>}/>
-          </Routes>
-        </Router>
-        
+        <KomodoraPresskit/>
       </ThemeProvider>
     </AppProvider>
   )
